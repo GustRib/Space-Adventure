031a0de2673b5ad9a102de30328778fbf658fde0
gust_rib <gu_crv1@hotmail.com>
2022-11-10 11:26:23 -0300

    adicionando as funcoes disponibilizadas pelo professor

--- scripts.js
+++ scripts.js
@@ -20,8 +20,126 @@
 // controla a cena
 // controla a cena
 var currentScene;
- 
+
 // contador de estrelas
+
+//1) Contabilize o número de colisões entre o objeto referente à "nave" e ao "asteróide".
+
+Mover.prototype.display = function() {
+    stroke(0);
+    strokeWeight(2);
+    fill(127);
+    ellipse(this.position.x, this.position.y, 30, 30);
+
+    if(mouseX > this.position.x-10 && mouseX < this.position.x+10 && mouseY > this.position.y-10 && mouseY < this.position.y+10 && bater)
+    {
+        bater = false;
+        vida--;
+    }
+    if(vida === 0)
+    {
+        drawScene3();
+    }
+
+  //3) Quando atingir 0 vidas, mude para cena 3, que deve ter um escrito "Game Over - Clique para jogar novamente". Clicando na tela da cena 3 o jogador volta à Cena 2 com os valores vida etc zerados (uma nova jogada).
+
+var drawScene3 = function() {
+    currentScene = 3;
+    background(0, 0, 0);
+    fill(255, 255, 255);
+    textSize(39);
+    text("Game Over", 65, height/2);
+    textSize(15);
+    text("Clique para jogar novamente", (width/2)-60, (height/2)+50);
+};
+
+//4) Crie um contador de tempo para o jogo, e crie um Ranking com os melhores tempos. Só precisa manter o Ranking dos 3 primeiros lugares.
+
+var drawScene2 = function() {
+    currentScene = 2;
+    background(173, 239, 255);
+    fill(7, 14, 145);
+    image(getImage("space/background"), 0,0, width, height);
+    fill(255, 255, 255);
+    textSize(15);
+    text("Número de vidas: " + vida, 10, 25);
+    text("Tempo: " + minutos + " :" + segundos + " :" + millisegundos , 150, 25);
+    
+    millisegundos++;
+    tempoTotal ++;
+
+        if(millisegundos === 100){
+            segundos++;
+            millisegundos = 0;
+        }
+
+        if(segundos === 60){
+            segundos = 0;
+            minutos++;
+        }
+
+        if(bater){
+            tempoBatida++;
+        }
+        else{
+            tempoBatida = 0;
+        }
+
+        if(asteroides === 0 && tempoBatida >= 3000){
+            asteroides = 1;
+            tempoBatida = 0;
+        }
+
+        else if(asteroides === 1 && tempoBatida >= 3000){
+            asteroides = 2;
+        }
+
+//continua o resto da função aqui.....
+
+//7) Crie os níveis: fácil, normal e difícil. Cada um dos níveis deve ter um nível de dificuldade. Por exemplo:  
+
+var drawScene2 = function() {
+
+//código anterior vem antes aqui
+
+//código para os níveis de dificuldade. É necessário criar as funções antes: var moverNivel2 = new Mover(); 
+//comentário - var moverNivel3 = new Mover();  var bouncing = new Bouncing(); e as demais
+
+    mover.update();
+    mover.checkEdges();
+    mover.display(); 
+
+    if(nivel >= 2){
+        moverNivel2.update();
+        moverNivel2.checkEdges();
+        moverNivel2.display(); 
+
+        if(nivel === 3){
+            moverNivel3.update();
+            moverNivel3.checkEdges();
+            moverNivel3.display(); 
+        }
+    }
+
+    if(asteroides >= 1){
+        mover2.update();
+        mover2.checkEdges();
+        mover2.display(); 
+
+        if(asteroides === 2){
+            mover3.update();
+            mover3.checkEdges();
+            mover3.display(); 
+        }
+    }
+
+    if(nivel === 3){
+    bouncing.update();
+    bouncing.checkEdges();
+    bouncing.display(); 
+    }
+};
+
 var count=0;
 var contador=0;
 var MAX_HEALTH = 5,
@@ -123,18 +241,18 @@ var makeStars = function() {
     //println(count);
 };
 /* Objeto Mover */
- 
+
 var Mover = function() {
 //  this.position = new PVector(random(width), random(height));
-  this.position = new PVector(500, random(height));
-  this.velocity = new PVector(random(-25, -1), random(-0.4, -0.4));
-  // adicionando acelerando crescente mas constante ao objeto
-  this.acceleration = new PVector(-0.001,0.005);
-  //this.acceleration = new PVector(-0.003,-0.00001);
-  // limite de velocidade
-  this.topspeed = 10;    
+    this.position = new PVector(500, random(height));
+    this.velocity = new PVector(random(-25, -1), random(-0.4, -0.4));
+    // adicionando acelerando crescente mas constante ao objeto
+    this.acceleration = new PVector(-0.001,0.005);
+    //this.acceleration = new PVector(-0.003,-0.00001);
+    // limite de velocidade
+    this.topspeed = 10;    
 };
- 
+
 Mover.prototype.update = function() {
     // adicionando a aceleração à velocidade que já temos
     this.velocity.add(this.acceleration);
@@ -146,33 +264,32 @@ Mover.prototype.update = function() {
     this.position.add(this.velocity);
 
 };
- 
+
 Mover.prototype.display = function() {
-  fill(29, 247, 5);
-  ellipse((this.position.x),(this.position.y), 50, 50);
+    fill(29, 247, 5);
+    ellipse((this.position.x),(this.position.y), 50, 50);
 };
- 
+
 Mover.prototype.checkEdges = function() {
- 
-  if (this.position.x > width) {
-    this.position.x = 0;
-  }  
-  else if (this.position.x < 0) {
-    this.position.x = width;
-    //this.position.y = random(1,200);
-    //println(this.position.y + " " + this.position.x);
-  }
- 
-  if (this.position.y > height) {
-    this.position.y = 0;
-  }  
-  else if (this.position.y < 0) {
-    this.position.y = height;
-  }
+    if (this.position.x > width) {
+        this.position.x = 0;
+    }  
+    else if (this.position.x < 0) {
+        this.position.x = width;
+        //this.position.y = random(1,200);
+        //println(this.position.y + " " + this.position.x);
+    }
+    
+    if (this.position.y > height) {
+        this.position.y = 0;
+    }  
+    else if (this.position.y < 0) {
+        this.position.y = height;
+    }
 };
- 
- Mover.prototype.bateu = function(){
-   if(mouseX > this.position.x && mouseY < this.position.y)
+
+Mover.prototype.bateu = function(){
+    if(mouseX > this.position.x && mouseY < this.position.y)
     {
         contador++;
         println("Bateu: "+ contador);
@@ -198,40 +315,40 @@ var drawScene2 = function() {
     fill(7, 14, 145);
     image(getImage("space/background"), 0,0, width, height);
     
-     
+    
     // desenhar estrelas
     //makeStars();
-     
+    
     // desenha a nave e a desloca na tela seguindo o mouse
     image(getImage("space/rocketship"), mouseX-30, mouseY-30, 60,60);
-     
+    
     // crie ou chama uma função que desenhe e mova 1 objeto
-      mover.update();
-      mover.checkEdges();
-      mover.display(); 
-      
+    mover.update();
+    mover.checkEdges();
+    mover.display(); 
+
     // crie um função de aceleração do objeto
         mover.bateu();
 };
- 
- 
+
+
 var drawScene3 = function() {
     currentScene = 3;
     background(173, 239, 255);
     println (" GAME OVER ");
 };
- 
+
 // clicou no mouse, avança cena
 mouseClicked = function() {
     if (currentScene === 1) {
         drawScene2();
-   } else if (currentScene === 2) {
+    } else if (currentScene === 2) {
         drawScene3();
     } else if (currentScene === 3) {
         drawScene1();
     }
 };
- 
+
 // funcao draw que desenha na tela.
 draw = function() {
     if (count < 300 && currentScene === 1) {
@@ -242,6 +359,7 @@ draw = function() {
         drawScene2();
     }
 };
- 
+
 // vamos começar pela Cena 1
 drawScene1();
+}
